d8cdfabaed7644779e99357f2ed47295d7f7de20
Aurora Scharff <aurora.sofie@gmail.com>
2024-10-17 16:55:08 +0200

    update

--- app/(intro)/client-server/_components/ClientComponent.tsx
+++ app/(intro)/client-server/_components/ClientComponent.tsx
@@ -5,9 +5,10 @@ import React from 'react';
 type Props = {
   children?: React.ReactNode;
   content?: React.ReactNode;
+  mutateData: () => Promise<string>;
 };
 
-export default function ClientComponent({ children, content }: Props) {
+export default function ClientComponent({ children, content, mutateData }: Props) {
   // Without "use client" we cannot use onClick, useState, useEffect, window.location etc
   return (
     <div className="border-2 border-red-500 p-4">
@@ -15,8 +16,9 @@ export default function ClientComponent({ children, content }: Props) {
       {content}
       {children}
       <button
-        onClick={() => {
-          return alert('ServerComponent button clicked');
+        onClick={async () => {
+          const data = await mutateData();
+          return alert(data);
         }}
       >
         Click
--- app/(intro)/client-server/page.tsx
+++ app/(intro)/client-server/page.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 
+import { prisma } from '@/db';
 import ClientComponent from './_components/ClientComponent';
 import ServerComponent from './_components/ServerComponent';
 
 export default function ClientServerPage() {
+  async function mutateData() {
+    'use server';
+
+    const data = await prisma.contact.findMany();
+    return data[0].first as string;
+  }
+
   return (
     <div>
       ClientServerPage
-      <ClientComponent content={<ServerComponent />}>
+      <ClientComponent mutateData={mutateData} content={<ServerComponent />}>
         <ServerComponent />
       </ClientComponent>
     </div>
