12a55d0d5ec27ef288f49780e2eb0cd745050a9f
Aurora Scharff <aurora.sofie@gmail.com>
2024-10-17 15:13:22 +0200

    update

--- app/(intro)/client-server/_components/ClientComponent.tsx
+++ app/(intro)/client-server/_components/ClientComponent.tsx
@@ -2,10 +2,18 @@
 
 import React from 'react';
 
-export default function ClientComponent() {
+type Props = {
+  children?: React.ReactNode;
+  content?: React.ReactNode;
+};
+
+export default function ClientComponent({ children, content }: Props) {
+  // Without "use client" we cannot use onClick, useState, useEffect, window.location etc
   return (
     <div className="border-2 border-red-500 p-4">
       ClientComponent
+      {content}
+      {children}
       <button
         onClick={() => {
           return alert('ServerComponent button clicked');
