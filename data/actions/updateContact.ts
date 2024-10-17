005ccebd3cd54ff6999133441d531fda0573c94c
Aurora Scharff <aurora.sofie@gmail.com>
2024-10-17 17:26:53 +0200

    Update

--- data/actions/updateContact.ts
+++ data/actions/updateContact.ts
@@ -3,12 +3,23 @@
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { prisma } from '@/db';
+import { slow } from '@/utils/slow';
 
 export async function updateContact(contactId: string, formData: FormData) {
+  await slow();
+
   const data = Object.fromEntries(formData);
 
   await prisma.contact.update({
-    data,
+    data: {
+      avatar: data.avatar as string,
+      email: data.email as string,
+      first: data.first as string,
+      github: data.github as string,
+      last: data.last as string,
+      notes: data.notes as string,
+      position: data.position as string,
+    },
     where: { id: contactId },
   });
 
